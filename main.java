ed798822e89369c0f6154754a345cc42d479eb56
MaaTh4 <127776316+MaaTh4@users.noreply.github.com>
2023-10-23 08:40:56 -0300

    Update main.java

--- main.java
+++ main.java
@@ -11,23 +11,27 @@ public class Main {
     public static void main(String[] args) {
 
 
-//        JFrame window = new JFrame("Exemplo Swing");
-//        window.setSize(400,200);
-//
-//        JButton botao = new JButton("Clique-me");
-//        botao.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent e) {
-//                System.out.println("O botão foi clicado");
-//            }
-//        });
-//
-//        window.add(botao);
-//        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//        window.setVisible(true);
+//JFrame
+        
+        JFrame window = new JFrame("Exemplo Swing");
+        window.setSize(400,200);
+
+        JButton botao = new JButton("Clique-me");
+        botao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("O botão foi clicado");
+           }
+        });
+
+        window.add(botao);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        window.setVisible(true);
 
 
+//JTextField
+        
         JFrame frame = new JFrame("Exemplo JTextField");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,150);
@@ -62,5 +66,87 @@ public class Main {
         });
 
         frame.setVisible(true);
+
+//COMBOBOX
+        
+                JFrame frame = new JFrame("Exemplo JComboBox");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,150);
+
+        JPanel panel = new JPanel();
+        frame.add(panel);
+
+        JLabel label = new JLabel("Selecione um mês");
+        panel.add(label);
+
+        String[] meses = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+
+        JComboBox<String> comboBox = new JComboBox<>(meses);
+        panel.add(comboBox);
+
+        JButton button = new JButton("Exibir Mês Selecionado");
+        panel.add(button);
+
+        JTextField textField = new JTextField(15);
+        panel.add(textField);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String mesSelecionado = (String) comboBox.getSelectedItem();
+
+                textField.setText("Mês Selecionado " + mesSelecionado);
+            }
+        });
+
+        frame.setVisible(true);
+
+
+        //RadioButton
+
+        JFrame frame = new JFrame("Exemplo JComboBox");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(300,150);
+
+            frame.setLocationRelativeTo(null);
+
+            JPanel panel = new JPanel();
+            frame.add(panel);
+
+            JLabel label = new JLabel("Você gosta de programação Java?");
+            panel.add(label);
+
+            JRadioButton simRadioButton = new JRadioButton("Sim");
+            JRadioButton naoRadioButton = new JRadioButton("Não");
+
+            ButtonGroup grupoOpcoes = new ButtonGroup();
+            grupoOpcoes.add(simRadioButton);
+            grupoOpcoes.add(naoRadioButton);
+
+            panel.add(simRadioButton);
+            panel.add(naoRadioButton);
+
+            JButton button = new JButton("Exibir Opção Selecionada");
+            panel.add(button);
+
+            JTextField textField = new JTextField(15);
+            panel.add(textField);
+
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String opcaoSelecionada = "";
+
+                    if(simRadioButton.isSelected()){
+                        opcaoSelecionada = "Sim";
+                    } else if (naoRadioButton.isSelected()) {
+                        opcaoSelecionada = "Não";
+                    }
+
+                    textField.setText("Opção Selecionada: " + opcaoSelecionada);
+                }
+            });
+
+            frame.setVisible(true);
     }
 }
